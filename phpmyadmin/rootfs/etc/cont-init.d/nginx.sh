2408ca43de3c7dcf510a8d81f0a61daa7ce45852
MasterCATZ <MasterCATZ@hotmail.com>
2022-06-05 14:37:29 +1000

    Increase Upload Limit
    Could not Import my 8Gb Files

--- phpmyadmin/rootfs/etc/cont-init.d/nginx.sh
+++ phpmyadmin/rootfs/etc/cont-init.d/nginx.sh
@@ -5,7 +5,7 @@
 # ==============================================================================
 declare upload_limit
 
-upload_limit="64M"
+upload_limit="64G"
 
 if bashio::config.has_value 'upload_limit'; then
     upload_limit=$(bashio::config "upload_limit")M
